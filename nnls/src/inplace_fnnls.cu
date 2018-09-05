a3188d36333951844279910c1a9c3e68c61b2344
Marco <m.barbone@outlook.it>
2018-09-05 11:21:20 +0200

    cleaned some stuff and added few comments

--- nnls/src/inplace_fnnls.cu
+++ nnls/src/inplace_fnnls.cu
@@ -5,6 +5,7 @@
 using namespace Eigen;
 
 
+// cache optimized matrix multiplication for 10x10 matrices
 #ifdef __CUDA_ARCH__
 __device__ __host__ 
 #endif
@@ -24,6 +25,162 @@ inline FixedMatrix transpose_multiply(const FixedMatrix &A){
   return result;
 }
 
+#ifdef __CUDA_ARCH__
+__device__ __host__ 
+#endif
+void inplace_fnnls(const FixedMatrix& A,
+                                       const FixedVector& b,
+                                       FixedVector& x,
+                                       const double eps,
+                                       const unsigned int max_iterations) {
+  // Fast NNLS (fnnls) algorithm as per
+  // http://users.wfu.edu/plemmons/papers/Chennnonneg.pdf
+  // page 8
+
+  // FNNLS memorizes the A^T * A and A^T * b to reduce the computation.
+  // The pseudo-inverse obtained has the same numerical problems so
+  // I keep the same decomposition utilized for NNLS.
+
+  // pseudoinverse (A^T * A)^-1 * A^T
+  // this pseudo-inverse has numerical issues
+  // in order to avoid that I substituted the pseudoinverse with the llT 
+  // this require some modification the projection (A^T * A)^P
+  // decomposition
+
+  // NNLS initialization
+  auto nPassive = 0;
+  
+  FixedMatrix AtA = transpose_multiply(A);
+  FixedVector Atb = A.transpose() *b;
+
+  FixedVector s;
+  FixedVector w;
+
+  Eigen::PermutationMatrix<VECTOR_SIZE> permutation;
+  permutation.setIdentity();
+
+// main loop
+#pragma unroll VECTOR_SIZE
+  for (auto iter = 0; iter < max_iterations; ++iter) {
+    const auto nActive = VECTOR_SIZE - nPassive;
+
+#ifdef DEBUG_FNNLS_CPU
+    cout << "iter " << iter << endl;
+#endif
+    
+    // If all the positivity contraints are satisfied the correct solution has 
+    // been calculated, then exit.
+    if(!nActive)
+      break;
+
+    w.tail(nActive) = Atb.tail(nActive) - (AtA * x).tail(nActive);
+
+#ifdef DEBUG_FNNLS_CPU
+    cout << "w" << endl << w.tail(nActive) << endl;
+#endif
+    // get the index of w that gives the maximum gain
+    Index w_max_idx;
+    const auto max_w = w.tail(nActive).maxCoeff(&w_max_idx);
+
+    // check for convergence
+    if (max_w < eps)
+      break;
+#ifdef DEBUG_FNNLS_CPU
+    cout << "n active " << nActive << endl;
+    cout << "w max idx " << w_max_idx << endl;
+  #endif
+
+    // need to translate the index into the right part of the vector
+    w_max_idx += nPassive;
+
+    // swap AtA to avoid copy
+    AtA.col(nPassive).swap(AtA.col(w_max_idx));
+    AtA.row(nPassive).swap(AtA.row(w_max_idx));
+    // swap Atb to match with AtA
+    Eigen::numext::swap(Atb.coeffRef(nPassive), Atb.coeffRef(w_max_idx));
+    Eigen::numext::swap(x.coeffRef(nPassive), x.coeffRef(w_max_idx));
+    // swap the permutation matrix to reorder the solution in the end
+    Eigen::numext::swap(permutation.indices()[nPassive],
+                        permutation.indices()[w_max_idx]);
+
+    ++nPassive;
+
+#ifdef DEBUG_FNNLS_CPU
+    cout << "max index " << w_max_idx << endl;
+    std::cout << "n_active " << nActive << std::endl;
+#endif
+
+// inner loop
+#pragma unroll VECTOR_SIZE
+    while (nPassive > 0) {
+      // calculating the pseudoinverse ((A^T * A)^P)^-1 via llt decomposition
+      s.head(nPassive) =
+          AtA.topLeftCorner(nPassive, nPassive).llt().solve(Atb.head(nPassive));
+
+      // If all the components are positive the solution has been calculated, 
+      // then exit
+      if (s.head(nPassive).minCoeff() > 0.) {
+        x.head(nPassive) = s.head(nPassive);
+        break;
+      }
+
+#ifdef DEBUG_FNNLS_CPU
+      cout << "s" << endl << s.head(nPassive) << endl;
+#endif
+      // Compute the ratio to move a negative component to 0 
+      auto alpha = std::numeric_limits<double>::max();
+      Index alpha_idx = 0;
+
+#pragma unroll VECTOR_SIZE
+      for (auto i = 0; i < nPassive; ++i) {
+        if (s[i] <= 0.) {
+          auto const ratio = x[i] / (x[i] - s[i]);
+          if (ratio < alpha) {
+            alpha = ratio;
+            alpha_idx = i;
+          }
+        }
+      }
+      // If no ratio can be derived all the components are not negative, exit
+      if (std::numeric_limits<double>::max() == alpha) {
+        x.head(nPassive) = s.head(nPassive);
+        break;
+      }
+
+#ifdef DEBUG_FNNLS_CPU
+
+      cout << "alpha " << alpha << endl;
+
+      cout << "x before" << endl << x << endl;
+
+#endif
+      // translate the solution toward the positive part
+      x.head(nPassive) += alpha * (s.head(nPassive) - x.head(nPassive));
+      // This index will became 0 is set to this value to avoid numerical problems
+      x[alpha_idx] = 0;
+      --nPassive;
+
+#ifdef DEBUG_FNNLS_CPU
+      cout << "x after" << endl << x << endl;
+#endif
+      // new components sutisfy the non negativity contraints so is moved 
+      // inside the active set
+      AtA.col(nPassive).swap(AtA.col(alpha_idx));
+      AtA.row(nPassive).swap(AtA.row(alpha_idx));
+      // swap Atb to match with AtA
+      Eigen::numext::swap(Atb.coeffRef(nPassive), Atb.coeffRef(alpha_idx));
+      Eigen::numext::swap(x.coeffRef(nPassive), x.coeffRef(alpha_idx));
+      // swap the permutation matrix to reorder the solution in the end
+      Eigen::numext::swap(permutation.indices()[nPassive],
+                          permutation.indices()[alpha_idx]);
+    }
+  }
+  // reshuffle the solution to the original order
+  x = x.transpose() * permutation.transpose();  
+}
+
+
+/*
 #ifdef __CUDA_ARCH__
 __global__
 #endif
@@ -66,168 +223,4 @@ void matrixMultiplication(const FixedMatrix &A, FixedMatrix &result){
 
 #endif
 
-#ifdef __CUDA_ARCH__
-__device__ __host__ 
-#endif
-void inplace_fnnls(const FixedMatrix& A,
-                                       const FixedVector& b,
-                                       FixedVector& x,
-                                       const double eps,
-                                       const unsigned int max_iterations) {
-  // Fast NNLS (fnnls) algorithm as per
-  // http://users.wfu.edu/plemmons/papers/Chennnonneg.pdf
-  // page 8
-
-  // FNNLS memorizes the A^T * A and A^T * b to reduce the computation.
-  // The pseudo-inverse obtained has the same numerical problems so
-  // I keep the same decomposition utilized for NNLS.
-
-  // pseudoinverse (A^T * A)^-1 * A^T
-  // this pseudo-inverse has numerical issues
-  // in order to avoid that I substituted the pseudoinverse whit the QR
-  // decomposition
-
-  // I'm substituting the vectors P and R that represents active and passive set
-  // with a boolean vector: if active_set[i] the i belogns to R else to P
-
-  // bool active_set[VECTOR_SIZE];
-  // memset(active_set, true, VECTOR_SIZE * sizeof(bool));
-
-
-  auto nPassive = 0;
-  
-  /*
-  #ifdef __CUDA_ARCH__
-  FixedMatrix AtA;
-  matrixMultiplication(A, AtA);
-  __syncthreads();
-  cudaDeviceSynchronize();
-  for(auto i = 0; i < MATRIX_SIZE; ++i){
-    for(auto j = 0; j < MATRIX_SIZE; ++j)
-      printf("%f ", AtA.data()[j* MATRIX_SIZE + i]);
-    printf("\n");
-  }
-  #else
-  FixedMatrix AtA = transpose_multiply(A);
-  #endif
-  assert(AtA == A.transpose() * A);
-  */
-  FixedMatrix AtA = transpose_multiply(A);
-  // FixedMatrix AtA = A.transpose() * A;
-  FixedVector Atb = A.transpose() *b;
-
-  FixedVector s;
-  FixedVector w;
-
-  Eigen::PermutationMatrix<VECTOR_SIZE> permutation;
-  permutation.setIdentity();
-
-// main loop
-#pragma unroll VECTOR_SIZE
-  for (auto iter = 0; iter < max_iterations; ++iter) {
-    const auto nActive = VECTOR_SIZE - nPassive;
-
-#ifdef DEBUG_FNNLS_CPU
-    cout << "iter " << iter << endl;
-#endif
-    
-    if(!nActive)
-      break;
-
-    w.tail(nActive) = Atb.tail(nActive) - (AtA * x).tail(nActive);
-
-#ifdef DEBUG_FNNLS_CPU
-    cout << "w" << endl << w.tail(nActive) << endl;
-#endif
-    // get the index of w that gives the maximum gain
-    Index w_max_idx;
-    const auto max_w = w.tail(nActive).maxCoeff(&w_max_idx);
-
-    // check for convergence
-    if (max_w < eps)
-      break;
-
-    // cout << "n active " << nActive << endl;
-    // cout << "w max idx " << w_max_idx << endl;
-
-    // need to translate the index into the right part of the vector
-    w_max_idx += nPassive;
-
-    // swap AtA to avoid copy
-    AtA.col(nPassive).swap(AtA.col(w_max_idx));
-    AtA.row(nPassive).swap(AtA.row(w_max_idx));
-    // swap Atb to match with AtA
-    Eigen::numext::swap(Atb.coeffRef(nPassive), Atb.coeffRef(w_max_idx));
-    Eigen::numext::swap(x.coeffRef(nPassive), x.coeffRef(w_max_idx));
-    // swap the permutation matrix to reorder the solution in the end
-    Eigen::numext::swap(permutation.indices()[nPassive],
-                        permutation.indices()[w_max_idx]);
-
-    ++nPassive;
-
-#ifdef DEBUG_FNNLS_CPU
-    cout << "max index " << w_max_idx << endl;
-    std::cout << "n_active " << nActive << std::endl;
-#endif
-
-// inner loop
-#pragma unroll VECTOR_SIZE
-    while (nPassive > 0) {
-      s.head(nPassive) =
-          AtA.topLeftCorner(nPassive, nPassive).llt().solve(Atb.head(nPassive));
-
-      if (s.head(nPassive).minCoeff() > 0.) {
-        x.head(nPassive) = s.head(nPassive);
-        break;
-      }
-
-#ifdef DEBUG_FNNLS_CPU
-      cout << "s" << endl << s.head(nPassive) << endl;
-#endif
-
-      auto alpha = std::numeric_limits<double>::max();
-      Index alpha_idx = 0;
-
-#pragma unroll VECTOR_SIZE
-      for (auto i = 0; i < nPassive; ++i) {
-        if (s[i] <= 0.) {
-          auto const ratio = x[i] / (x[i] - s[i]);
-          if (ratio < alpha) {
-            alpha = ratio;
-            alpha_idx = i;
-          }
-        }
-      }
-
-      if (std::numeric_limits<double>::max() == alpha) {
-        x.head(nPassive) = s.head(nPassive);
-        break;
-      }
-
-#ifdef DEBUG_FNNLS_CPU
-
-      cout << "alpha " << alpha << endl;
-
-      cout << "x before" << endl << x << endl;
-
-#endif
-
-      x.head(nPassive) += alpha * (s.head(nPassive) - x.head(nPassive));
-      x[alpha_idx] = 0;
-      --nPassive;
-
-#ifdef DEBUG_FNNLS_CPU
-      cout << "x after" << endl << x << endl;
-#endif
-      AtA.col(nPassive).swap(AtA.col(alpha_idx));
-      AtA.row(nPassive).swap(AtA.row(alpha_idx));
-      // swap Atb to match with AtA
-      Eigen::numext::swap(Atb.coeffRef(nPassive), Atb.coeffRef(alpha_idx));
-      Eigen::numext::swap(x.coeffRef(nPassive), x.coeffRef(alpha_idx));
-      // swap the permutation matrix to reorder the solution in the end
-      Eigen::numext::swap(permutation.indices()[nPassive],
-                          permutation.indices()[alpha_idx]);
-    }
-  }
-  x = x.transpose() * permutation.transpose();  
-}
+*/
